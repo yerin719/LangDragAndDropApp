b35e3128eb1cbbaa8a9c1a60ab8f1961fe51df2e
이예린 <yerin@iyelin-ui-MacBookPro.local>
2025-05-07 10:34:09 +0900

    Drag and Drop Project part Three

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Animated, Easing, PanResponder, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import { Ionicons } from '@expo/vector-icons';
+import icons from './icons';
 
 const BLACK_COLOR = '#1e272e';
 const GREY = '#485460';
@@ -105,7 +106,7 @@ export default function App() {
               easing: Easing.linear,
               useNativeDriver: true,
             }),
-          ]).start();
+          ]).start(nextIcon);
         } else {
           Animated.parallel([onPressOut, goHome]).start();
         }
@@ -113,6 +114,14 @@ export default function App() {
     })
   ).current;
   // State
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    setIndex(prev => prev + 1);
+    Animated.parallel([
+      Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(opacity, { toValue: 1, useNativeDriver: true }),
+    ]).start();
+  };
   return (
     <Container>
       <Edge>
@@ -128,7 +137,7 @@ export default function App() {
             transform: [...position.getTranslateTransform(), { scale }],
           }}
         >
-          <Ionicons name="beer" color={GREY} size={76} />
+          <Ionicons name={icons[index]} color={GREY} size={76} />
         </IconCard>
       </Center>
       <Edge>
