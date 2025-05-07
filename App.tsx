9c700ab5298ea4e3d26dfe9f653d1697261c5b4b
이예린 <yerin@iyelin-ui-MacBookPro.local>
2025-05-07 10:11:42 +0900

    Drag and Drop Project part One

--- App.tsx
+++ App.tsx
@@ -1,33 +1,103 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, PanResponder, Text, View } from 'react-native';
 import styled from 'styled-components/native';
-import { SafeAreaView, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const Container = styled(SafeAreaView)`
+const BLACK_COLOR = '#1e272e';
+const GREY = '#485460';
+const GREEN = '#2ecc71';
+const RED = '#e74c3c';
+
+const Container = styled.View`
   flex: 1;
-  background-color: #f5f5f5;
+  background-color: ${BLACK_COLOR};
 `;
-
-const Content = styled(View)`
+const Edge = styled.View`
   flex: 1;
-  padding: 20px;
   justify-content: center;
   align-items: center;
 `;
-
-const Title = styled(Text)`
-  font-size: 24px;
-  font-weight: bold;
-  color: #333;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
 `;
 
-function App(): React.JSX.Element {
+const Word = styled.Text`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${props => props.color};
+`;
+const Center = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+export default function App() {
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goHome = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderRelease: () => {
+        Animated.parallel([onPressOut, goHome]).start();
+      },
+    })
+  ).current;
+  // State
+
   return (
     <Container>
-      <Content>
-        <Title>Welcome to My App</Title>
-      </Content>
+      <Edge>
+        <WordContainer>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}
+        >
+          <Ionicons name="beer" color={GREY} size={76} />
+        </IconCard>
+      </Center>
+      <Edge>
+        <WordContainer>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
     </Container>
   );
 }
-
-export default App;
